5ed42ce720befdaae00ebec7134620ac550026a5
tianminghui <tianmh2013@163.com>
2024-04-19 14:25:11 +0800

    feat:升级openai到最新版

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,8 @@
 import traceback
 import openai
+from openai import OpenAI
+
+client = None
 import sys
 import json
 import time
@@ -11,16 +14,14 @@ from loguru import logger
 def single_message_test(**kwargs):
     print(f"----------single message test {kwargs}----------")
     try:
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[{"role": "user", "content": "你好"}],
-            **kwargs,
-        )
+        completion = client.chat.completions.create(model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": "你好"}],
+        **kwargs)
         if kwargs.get("stream"):
             for chunk in completion:
-                print(json.dumps(chunk, ensure_ascii=False))
+                print(chunk)
         else:
-            print(json.dumps(completion, ensure_ascii=False))
+            print(completion)
     except openai.APIError as e:
         #Handle API error here, e.g. retry or log
         print(f"OpenAI API returned an API Error: {e}")
@@ -35,21 +36,19 @@ def single_message_test(**kwargs):
 def multiple_messages_test(**kwargs):
     print(f"----------multiple messages test {kwargs}----------")
     try:
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "你是一个旅行专家, 能够帮我们制定旅行计划"},
-                {"role": "user", "content": "你好"},
-                {"role": "assistant", "content": "你好!很高兴认识你。"},
-                {"role": "user", "content": "你是谁？"},
-            ],
-            **kwargs,
-        )
+        completion = client.chat.completions.create(model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "你是一个旅行专家, 能够帮我们制定旅行计划"},
+            {"role": "user", "content": "你好"},
+            {"role": "assistant", "content": "你好!很高兴认识你。"},
+            {"role": "user", "content": "你是谁？"},
+        ],
+        **kwargs)
         if kwargs.get("stream"):
             for chunk in completion:
-                print(json.dumps(chunk, ensure_ascii=False))
+                print(chunk)
         else:
-            print(json.dumps(completion, ensure_ascii=False))
+            print(completion)
     except Exception as e:
         s = traceback.format_exc()
         print(s)
@@ -59,12 +58,11 @@ def multiple_messages_test(**kwargs):
 
 if __name__ == "__main__":
     api_key = sys.argv[1]
-    openai.api_base = "http://localhost:8090/v1"
-    openai.api_key = api_key
+    client = OpenAI(api_key=api_key, base_url="http://localhost:8090/v1")
     # single_message_test()
-    time.sleep(2)
-    single_message_test(stream=True)
     # time.sleep(2)
-    # multiple_messages_test()
+    # single_message_test(stream=True)
+    # time.sleep(2)
+    multiple_messages_test()
     time.sleep(2)
-    # multiple_messages_test(stream=True)
+    multiple_messages_test(stream=True)
